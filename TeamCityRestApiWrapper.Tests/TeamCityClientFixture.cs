0d66ff51ba40c25459eca4871351c6ca388754fd
Kauflin, Michael <michael.kauflin@sonova.com>
2016-04-21 14:04:06 +0200

    enable NuGet package restore add appveyor.yml

---
+++ TeamCityRestApiWrapper.Tests/TeamCityClientFixture.cs
@@ -0,0 +1,28 @@
+﻿namespace TeamCity.RestApi.Wrapper.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using RestSharp;
+
+    [TestFixture]
+    public class TeamCityClientFixture
+    {
+        [TestCase("http://teamcity.codebetter.com/", ExpectedResult = "http://teamcity.codebetter.com/")]
+        [TestCase("", ExpectedResult = "http://teamcity.codebetter.com/")]
+        [TestCase(null, ExpectedResult = "http://teamcity.codebetter.com/")]
+        public string InitClientTest(string url)
+        {
+            var teamCityClient = string.IsNullOrEmpty(url) ? new TeamCityClient() : new TeamCityClient(url);
+            RestClient restClient = teamCityClient.RestClient;
+
+            Assert.That(restClient, Is.Not.Null);
+
+            Uri baseUrl = restClient.BaseUrl;
+            Console.WriteLine(baseUrl);
+
+            return baseUrl.AbsoluteUri;
+        }
+    }
+}
---
+++ TeamCityRestApiWrapper/TeamCityClient.cs
@@ -0,0 +1,38 @@
+﻿namespace TeamCity.RestApi.Wrapper
+{
+    using System;
+
+    using RestSharp;
+
+    public class TeamCityClient
+    {
+        private readonly RestClient _restClient;
+
+        public TeamCityClient() : this(GetRestClient())
+        {}
+
+        public TeamCityClient(string url) : this(new RestClient(url))
+        { }
+
+        public TeamCityClient(RestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public RestClient RestClient { get { return _restClient; }}
+
+        private static RestClient GetRestClient()
+        {
+            string user = Environment.GetEnvironmentVariable("TeamCity.User");
+            string pwd = Environment.GetEnvironmentVariable("TeamCity.Pwd");
+            string url = Environment.GetEnvironmentVariable("TeamCity.Url");
+
+            if (!string.IsNullOrEmpty(url))
+            {
+                return new RestClient(url);
+            }
+            
+            return new RestClient("http://teamcity.codebetter.com/");
+        }
+    }
+}
